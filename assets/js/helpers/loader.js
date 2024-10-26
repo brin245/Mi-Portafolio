ea425fa7df6203395ad1cc1c7f89fe81748abe4a
BryanBautista <120752256+brin245@users.noreply.github.com>
2024-10-25 23:31:57 -0500

    cambio del loader

--- assets/js/helpers/loader.js
+++ assets/js/helpers/loader.js
@@ -1,9 +1,15 @@
 const loaderContainer = document.querySelector('.loader');
 
 function loader() {
-	window.addEventListener('load', function () {
+	// Intentar ocultar el loader cuando el HTML esté cargado
+	window.addEventListener('DOMContentLoaded', function () {
 		loaderContainer.classList.add('loader--hidden');
 	});
+
+	// Fallback: Ocultar el loader después de 3 segundos en caso de que el evento no funcione
+	setTimeout(() => {
+		loaderContainer.classList.add('loader--hidden');
+	}, 3000); // 3000 ms = 3 segundos
 }
 
 export default loader;
